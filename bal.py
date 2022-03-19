95d5f8862d4156db43a88d9b0fb6870cad408d7a
liams-code <99477913+liams-code@users.noreply.github.com>
2022-03-20 03:01:36 +0900

    Update bal.py

--- bal.py
+++ bal.py
@@ -1,8 +1,8 @@
 import pyupbit
 
 
-access = "pnOxTRGv2r6Ogmto3GP9rSOUvzGgVBPymlgxkHMM"
-secret = "xYkxvaEj2P6T3r6NtCiH8Rg5oJdAaQRNxQ92xF5s"
+access = ""
+secret = ""
 
 upbit = pyupbit.Upbit(access, secret)
 balance = upbit.get_balance("KRW")
